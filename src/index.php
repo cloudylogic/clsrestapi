98449cc0b080aa2baa7471e67f536accfa33b1a4
Ken Lowrie <klowrie@gmail.com>
2017-07-12 17:35:28 -0500

    Added a few more API links

--- src/index.php
+++ src/index.php
@@ -2,4 +2,12 @@
 echo "cls rest api";
 ?>
 <br />
-<a href="reels/index.php">reels</a>
+<a href="reels/">reels</a>
+<br />
+<a href="about-us/">about-us</a>
+<br />
+<a href="versions/">versions</a>
+<br />
+<a href="contact-info/">contact-info</a>
+<br />
+<a href="our-work/">our-work</a>
