472c38bafb47f72117979cdcad55b626b0853a62
Ken Lowrie <klowrie@gmail.com>
2017-07-12 21:24:41 -0500

    Updates when debugging
    Comment out the unused auth override code, which is intended for
    keeping live server credentials from being exposed on GitHub. Add a few
    debug messages to help figure out the weird error I was getting from
    the gulp-php-server module. Had to force it to v0.0.8 temporarily. I
    think I need to upgrade node to v5 on my laptop for better fix.

--- src/gulpfiles/phpserver.js
+++ src/gulpfiles/phpserver.js
@@ -1,7 +1,15 @@
+
 var gulp = require('gulp'),
 	browserSync = require('browser-sync'),
 	php = require('gulp-connect-php'),
 	fs = require('fs-extra');
+
+console.log("Loadmodules complete...\r\n");
+
+/*
+// This auth override code is used in environments where I need to specify valid credentials
+// for Wordpress and/or SQL login, but do not want them exposed since this code is pushlished
+// on GitHub. :)
 
 // helper function to build an FTP connection based on our configuration
 function checkForAuthOverride(authFilename) {
@@ -24,9 +32,13 @@ gulp.task('override-auth', function(){
 	checkForAuthOverride('sql.logininfo.php');
 });
 
+gulp.task('php', ['override-auth'], function() {
+*/
+
 var srcDir = '../../src/'
 
-gulp.task('php', ['override-auth'], function() {
+gulp.task('php', [], function() {
+	console.log("about to start php.server()\r\n");
     php.server({}, function () {
         browserSync({ proxy: '127.0.0.1:8000'});
     });
