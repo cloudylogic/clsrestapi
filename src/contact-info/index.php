a03ef378d3fa4be6053480112632d624f35b0934
Ken Lowrie <klowrie@gmail.com>
2017-07-13 07:09:46 -0500

    Added social media object and version object

--- src/contact-info/index.php
+++ src/contact-info/index.php
@@ -1,13 +1,27 @@
 <?php
 
+include_once("../includes/versions.php");
 include_once("../includes/creply.php");
+
+class cSocialMedia {
+	public $network;
+	public $id;
+	public $url;
+	
+	public function __construct($network, $id, $url){
+		$this->network = $network;
+		$this->id = $id;
+		$this->url = $url;
+	}
+}
 
 class cContactInfoReply extends cBaseReply {
 	public $location;
 	public $address;
 	public $email;
 	public $phone;
-	
+	public $socialNetworks;
+	public $apiVer;
 
 	public function __construct(){
 		parent::__construct();
@@ -25,6 +39,14 @@ class cContactInfoReply extends cBaseReply {
 		$this->email = "info@cloudylogic.com";
 		
 		$this->phone = "512.710.7257";
+		
+		$this->socialNetworks = Array();
+		$this->socialNetworks[] = new cSocialMedia("Facebook", "cloudylogic", "https://www.facebook.com/cloudylogic");
+		$this->socialNetworks[] = new cSocialMedia("Twitter", "cloudylogic", "https://twitter.com/cloudylogic");
+		$this->socialNetworks[] = new cSocialMedia("Vimeo", "cloudylogic", "https://vimeo.com/cloudylogic");
+		
+		$this->apiVer = new cAPIversion(CLSRESTAPI_VER_CONTACT_INFO_NAME,CLSRESTAPI_VER_CONTACT_INFO_API,CLSRESTAPI_VER_CONTACT_INFO_DATA);
+
 	}
 }
 
