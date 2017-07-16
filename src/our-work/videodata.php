015dd0fb6a8623e88e2b62ba29ce4e21cee3ae32
Ken Lowrie <klowrie@gmail.com>
2017-07-16 12:21:21 -0500

    Remove formatting from description strings
    Let the client decide on how to format this data.

--- src/our-work/videodata.php
+++ src/our-work/videodata.php
@@ -19,8 +19,8 @@ function getShowcaseVideos()
 	                           "Put the spotlight on your business with a high quality profile video, " .
 	                           "like this one we produced for BungoBox (www.bungobox.com), an innovative " .
 	                           "company that rents reusable moving boxes instead of putting more cardboard " .
-	                           "into the world.\n\nWe help you identify your strengths and then highlight " .
-	                           "them in a documentary-style movie.\n\nContact us today for a custom quote " .
+	                           "into the world. We help you identify your strengths and then highlight " .
+	                           "them in a documentary-style movie. Contact us today for a custom quote " .
 	                           "and we’ll help you tell YOUR story."
 	                           );
 
@@ -38,9 +38,9 @@ function getShowcaseVideos()
 	                           "band better than a performance video! And, for artists on a budget, they’re " .
 	                           "pretty economical compared to a standard video shoot. Plus, you’ll get the " .
 	                           "extra benefit of better locations at less expense, since many venues like " .
-	                           "the added exposure.\n\nCheck out this video we shot for Orlando’s SNAFU " .
+	                           "the added exposure. Check out this video we shot for Orlando’s SNAFU " .
 	                           "and their song “Dixie Sound Check”. We loved their energy and had a great " .
-	                           "time shooting and producing this performance.\n\nGet in touch today for a " .
+	                           "time shooting and producing this performance. Get in touch today for a " .
 	                           "custom quote on your next video!"
 	                           );
 	$showcaseVideos->addVideo( "SI.COM - H.S. Player of the Week", 
@@ -57,9 +57,9 @@ function getShowcaseVideos()
 	                           "doing Sports Documentary profiles for athletes at any level. Here’s one we " .
 	                           "shot for the Sports Illustrated High School Player of the Week series when " .
 	                           "they highlighted top prospect quarterback Jeff Driskel from Hagerty High " .
-	                           "School in Oviedo, Florida.\n\nWhether you’re preserving childhood memories " .
+	                           "School in Oviedo, Florida. Whether you’re preserving childhood memories " .
 	                           "or promoting a future career, these videos are a great way to document the " .
-	                           "journey.\n\nCall today and together we can figure out which approach is " .
+	                           "journey. Call today and together we can figure out which approach is " .
 	                           "right for you."
 	                           );
 	$showcaseVideos->addVideo( "Have Yourself A Merry Little Christmas", 
@@ -75,10 +75,10 @@ function getShowcaseVideos()
 	                           "Multi-Camera music videos capture another element of the performance. These " .
 	                           "high-end productions are styled to fit the music and the artist and involve " .
 	                           "plenty of pre-production to be sure that the final product matches the " .
-	                           "original concept.\n\nHere’s a simple multi-cam shoot we did for Philip R. " .
+	                           "original concept. Here’s a simple multi-cam shoot we did for Philip R. " .
 	                           "Bonanno’s “Have Yourself A Merry Little Christmas” performance inside the " .
-	                           "Cloudy Logic Studios Florida Complex.\n\nGive us a call and we can discuss " .
-	                           "ideas and options!\n\n(Original music by Hugh Martin, lyrics by Ralph Blane)"
+	                           "Cloudy Logic Studios Florida Complex. Give us a call and we can discuss " .
+	                           "ideas and options! (Original music by Hugh Martin, lyrics by Ralph Blane)"
 	                           );
 	$showcaseVideos->addVideo( "Sunsight AAT Training", 
 	                           "23581310", 
@@ -90,14 +90,14 @@ function getShowcaseVideos()
 	                           "", 
 	                           "Ken Lowrie &amp; Brenda Lowrie",
 	                           "Ken Lowrie &amp; Brenda Lowrie",
-	                           "Training Videos? Gotcha covered!\n\nCheck out these web-based training videos " .
+	                           "Training Videos? Gotcha covered! Check out these web-based training videos " .
 	                           "that we produced for Sunsight Instruments (www.sunsight.com), a high-tech " .
 	                           "company that designs and builds antenna positioning systems for mobile " .
-	                           "communications companies.\n\nThis was a full-service production, with script " .
+	                           "communications companies. This was a full-service production, with script " .
 	                           "development, product photography, video, voiceover, interactive software " .
 	                           "capture, and much more done completely in-house at Cloudy Logic Studios. " .
 	                           "Of course, we’re happy to handle as much or as little as your production " .
-	                           "needs.\n\nGive us a call and we can develop a plan to get your training " .
+	                           "needs. Give us a call and we can develop a plan to get your training " .
 	                           "program up and running, or update what you already have."
 	                           );
 	$showcaseVideos->addVideo( "Symantec Mini Cooper Spot", 
@@ -111,10 +111,10 @@ function getShowcaseVideos()
 	                           "Willie Sheely",
 	                           "Willie Sheely",
 	                           "Commercials and Promotional Videos are a great way to get exposure or " .
-	                           "highlight a promotion.\n\nThis promotional video for Symantec " .
+	                           "highlight a promotion. This promotional video for Symantec " .
 	                           "(www.symantec.com) was developed to highlight the benefits of their " .
-	                           "NetBackup and BackupExec software versus the competition.\n\nGet the " .
-	                           "advantage on your competition and engage your customers with video!\n\n" .
+	                           "NetBackup and BackupExec software versus the competition. Get the " .
+	                           "advantage on your competition and engage your customers with video! " .
 	                           "Call us today and we can design a plan to work with your budget, no matter " .
 	                           "how big or how small."
 	                           );
@@ -129,11 +129,11 @@ function getShowcaseVideos()
 	                           "Brenda Lowrie &amp; Ken Lowrie",
 	                           "Brenda Lowrie",
 	                           "Update your demo and book more clients! Show them what they’ll be missing " .
-	                           "if they don’t book you for the gig.\n\nHere’s an example of a basic Talent " .
+	                           "if they don’t book you for the gig. Here’s an example of a basic Talent " .
 	                           "Demo Reel we produced for Dane Colbert to send to potential clients. Dane’s " .
 	                           "huge catalog of cover songs and original tracks is perfect for a wide " .
 	                           "variety of events and showing his talents on video has helped increase his " .
-	                           "bookings.\n\nGet in touch today and we’ll help you get the exposure you " .
+	                           "bookings. Get in touch today and we’ll help you get the exposure you " .
 	                           "deserve, while respecting your budget."
 	                           );
 	$showcaseVideos->addVideo( "Phil Bonanno - Living", 
@@ -148,7 +148,7 @@ function getShowcaseVideos()
 	                           "Ken Lowrie",
 	                           "Single-camera music videos are less expensive overall than the multi-camera " .
 	                           "versions, yet they are still an extremely effective way to highlight your " .
-	                           "talent.\n\nThis is the official music video for &quot;Living&quot;, the first " .
+	                           "talent. This is the official music video for &quot;Living&quot;, the first " .
 	                           "track from Phil Bonanno&apos;s &quot;Short Songs From a Little Room EP&quot;. " .
 	                           "Phil wrote the songs, played all the instruments, and then recorded them all " .
 	                           "in the spare &quot;little room&quot; inside his home in downtown Orlando, " .
